af74da3e88789db5234b48490f3ee0c882bca2d3
AlexYang2000 <285501805@qq.com>
2016-02-14 21:52:35 +0800

    数组中键和值的取出 并将键名转为变量

---
+++ demo3.php
@@ -0,0 +1,17 @@
+<?php
+$products=array('apple'=>10,'milk'=>12,'pear'=>6);
+extract($products);
+echo $pear;
+echo $apple;
+echo $milk;
+print_r($products);
+array_unshift($products, '3');
+array_push($products, '8');
+array_shift($products);
+print_r($products);
+array_pop($products);
+print_r($products);
+$i=array_rand($products,2);
+echo($i[0]);
+echo($i[1]);
+?>
