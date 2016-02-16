a93931884bdaa7c9671bfcde489ea33a6f878dee
AlexYang2000 <285501805@qq.com>
2016-02-16 08:36:41 +0800

    目录的访问
    basename dirname  filename extension pathinfo 等函数的应用

--- basic6/demo1.php
+++ basic6/demo1.php
@@ -1,3 +1,20 @@
 <?php
-echo'directory';
+$path ='d:/doc/a.txt';
+echo $path;
+echo "<br/>";
+echo(basename($path));
+echo'<br/>';
+echo(dirname($path));
+echo "<br/>";
+$pathinfo=pathinfo($path);
+print_r($pathinfo);
+extract($pathinfo);
+echo "<br/>";
+echo "$filename";
+echo "<br/>";
+echo "$extension";
+echo "<br/>";
+echo "$dirname";
+echo "<br/>";
+echo "$basename";
 ?>
